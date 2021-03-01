fcda1cbc98ab0c5219d80301f175855ad2e67db7
Arthur Han <arthurhan269@gmail.com>
2021-03-01 20:22:31 -0300

    Add files via upload

---
+++ labyrinth/labyrinth.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "labyrinth.h"
+
+void loadmatrix(char* archive, int mat[T][T]){
+    
+  FILE *arq = fopen(archive, "r");
+  
+  for(int i = 0; i < T; i++)
+    for(int j =0; j < T; j++) fscanf(arq,"%d",&mat[i][j]);
+  fclose(arq);
+}
+
+void printmatrix(int mat[T][T]){
+  printf("[][][][][][][][][][][][]\n");
+  for(int i = 0; i < T; i++){
+    printf("[]");
+      for(int j = 0; j < T; j++){
+        if(i == 0 && j == 0) printf("👱‍️ ‍️");
+        else if(mat[i][j] == BLOCK) printf("[]");
+        else if(i == 9 && j == 9) printf("🏁");
+        else printf("  ");
+        if (j == 9 && i != 9) printf("[]");
+      }
+      printf("\n");
+  }
+  printf("[][][][][][][][][][][]\n");
+}
+
+int search(int mat[T][T],int lin,int col){
+    
+  int aux;
+  
+  if((lin >= T) || (lin < 0) || (col >= T) || (col < 0)) aux = 0;
+  else if(mat[lin][col] == BLOCK) aux = 0;
+  else if(lin == 9 && col == 9) aux = 1;
+  else{
+    mat[lin][col] = 1;
+    aux = search(mat,lin-1,col);
+    aux |= search(mat,lin+1,col);
+    aux |= search(mat,lin,col-1);
+    aux |= search(mat,lin,col+1);  
+  }
+  if(aux == 1)
+    printf("posição:[%d, %d]\n", lin, col);
+
+  return aux;
+}
---
+++ labyrinth/labyrinth.h
@@ -0,0 +1,7 @@
+#define T 10
+#define EMPTY 0
+#define BLOCK 1
+
+void loadmatrix(char* archive, int mat[T][T]);
+void printmatrix(int mat[T][T]);
+int search(int mat[T][T],int lin,int col);

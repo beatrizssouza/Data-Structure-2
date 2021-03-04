f6aefb6cfd29cbf7f89d4c85b9a3832a2ee95b82
Arthur Han <arthurhan269@gmail.com>
2021-03-04 19:06:23 -0300

    Update labyrinth.h

--- labyrinth/labyrinth.h
+++ labyrinth/labyrinth.h
@@ -1,3 +1,8 @@
+#ifndef LABYRINTH_H
+#define LABYRINTH_H
+
+#include <stdio.h>
+
 #define T 10
 #define EMPTY 0
 #define BLOCK 1
@@ -5,3 +10,5 @@
 void loadmatrix(char* archive, int mat[T][T]);
 void printmatrix(int mat[T][T]);
 int search(int mat[T][T],int lin,int col);
+
+#endif
